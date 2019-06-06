6107d6360ab9913f810503a9c3d4e3be05772573
SoftDev Student <softdevstudent@robotics-mba-06.local>
2019-06-06 09:04:48 -0400

    my version

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+//creating Emoji class
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+
+init(symbol: String, name: String, description: String,
+            usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
---
+++ EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by SoftDev Student on 6/4/19.
+//  Copyright © 2019 Belle L. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+        
+       
+    }
+    
+    
+    
+    
+}
